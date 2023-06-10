9f4faf9a3c0e607f473410c87fdcf71b4c19bf96
rokasjasinskas <125792805+rokasjasinskas@users.noreply.github.com>
2023-06-10 09:01:56 +0000

    Add lines to learning tool

--- learning_tool/learning_tool.py
+++ learning_tool/learning_tool.py
@@ -2,92 +2,62 @@ import re
 import sys
 import datetime
 
+class Question: 
+    def __init__(self, question_text, answer): 
+        self.question_text = question_text
+        self.answer = answer 
+        self.active = True
+        self.show_count = 0 
+        self.correct_count = 0
+        self.id = id
 
-
-# after input program has to get input in order to choose what what to do next. User should be able to choose one of these solutions: 
-    # Adding questions.
-    # Statistics viewing.
-    # Disable/enable questions.
-    # Practice mode.
-    # Test mode.
-    # (Bonus) Profile Select
-# Ask for input and test it if it is in library.     
-   
-
-# Adding questions mode: 
-# use class to describe question mode 
-    # def quiz questions 
-        #abc type. User decides input Questions and 3 answers A B C 
-    #def free-from questions 
-        # user give input Questions and Answer
-        # input stored in file 
-    # Questions should be saved on separate file so that once the program is close and opened again the questions remain same 
-    # user has to input 5 questions before moving forward. 
-    # use try len(questions) < 5: 
-    # Need to determine how file are stored 
         
+    def disable(self):
+        self.active = False 
 
-# Statistics viewing mode: 
-    # This mode should print out questions which are in the system. 
-    # Each questions should have: 
-        # ID number 
-        # Active questions or not
-        # The question text
-        # The number of times is was shown during practise or test 
-        # The percentage of time is was shoen correct
-s
-# Disable/Enable questions mode: 
-    # If user inputs questions ID code: 
-        # prompt message with  question and answer with question if this one should be enablad or disabled. 
-        # if disable it should not be used in practise or test stages 
-        # enable/disable should be stored in main file 
+    def enable(self): 
+        self.active = True 
 
-# Practise mode: 
-    # the mode there questions are given non-stop till hard stop
-    # the questions are chosen in such a way that the questions that answered should apear less likely. 
-    # This info should be stored in file with correct answered questions counted 
+    def id (self): 
+        return random.randrange(100, 999)
 
-# Test mode: 
-    # The mode there knowledge is tested 
-    # Number of questions is sellected with no more than current questions number. 
-    # Questions are sellected randomly, Only once 
-    # Result of test are shown with score 
-    # Results are stored in separate file results.txt with date and time added next to score 
-
-    
-        
-# def main: 
-    # Ask what solutions should proceed with input
-    # Check if input is in library 
-        # if answer is A: 
-            # try: 
-                # add questions
-            # except ctrl + c: 
-                # if lenght (questions) > 5 
-                    #sys.exit 
-                # else:
-                    # print message 
-                    # add questions
-    
-        # if answer is B: 
-            # statistics_viewing()
-        
-        # if answer is C: 
-            # disable_enable mode()
-    
-        # if answer is D: 
-            # practise_mode ()
-    
-        # if answer is E: 
-            # test_mode()
-    
-        
-# Technical requirements:
-    # use regular expresion
-    # use classes 
-    # use file i/o
-    # write 3 unit tests
-    # store output files in github 
-
+    def display_statistics (self): 
+        #has to be withou print 
+        percentage = self.corrrect_count / self.show_count * 100 if self.show_count .0 else 0 
+        print(f"ID: {self.id}")
+        print(f"Question: {self.question_text}")  
+        print(f"Answer: {self.answer}")
+        print(f"Active: {'Yes' if self.active else 'No'}")
+        print(f"Times Shown: {self.show_count}")
+        print(f"Percentage Correct: {percentage:.2f}%\n")
             
+class QuizQuestion (Question): 
+    def __init__(self.question_text, answer, options):
+        super().__init__ (self, question_text, answer): 
+        self.options = options 
+
+    def display_question (self): 
+        print(f"Question: {self.question_text}")
+        for index, option in enumerate.(self.options): 
+            print(f"{chr(65 + index)}. {option}")
+        print()
     
+    def check_answer (self, user_answer):
+        return user_answer.lower() == self.answer.lower()
+
+class FreeFormQuestion(Question): 
+    def display_question(self): 
+        print (f"Question: {self.question_text}\n")
+    
+    def check_anwer(self, user_answer): 
+        return user_answer.strip.lower() == self.answer.strip.lower()
+
+
+class InteractiveLearninTool: 
+    def __init__ (self): 
+        #store questions in file
+        self.questions = []
+        self.load_questions()
+            
+    def load_questions(self):
+        try: 
