ce86f0ba3185c6650a59636c99f968a54e40ef2c
rokasjasinskas <125792805+rokasjasinskas@users.noreply.github.com>
2023-06-20 10:43:22 +0000

    regex

--- CS50P/twitter.py
+++ CS50P/twitter.py
@@ -1,5 +1,6 @@
+import re
+
 url = input ("URL: ").strip()
 
-username = url.removeprefix("https://twitter.com/")
-
-print(f"Username: {username}") 
+if matches := re.search (r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE): 
+    print (f"Username:", matches.group(1))
