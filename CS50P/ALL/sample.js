dff295e7a056a4081bfe2845a82ead5f3e0dad45
Rokas <jasinskas.rokas@gmail.com>
2023-08-17 20:17:29 +0300

    fd

---
+++ CS50P/ALL/sample.js
@@ -0,0 +1,12 @@
+const wkArray = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const day = 4; // Change this value to the desired day index
+
+console.log(`${wkArray[day]} is day number ${parseInt(day) + 1} of the week!`);
