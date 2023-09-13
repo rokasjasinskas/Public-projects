ed1b7cfb7043905ecebc9a297c190dc6f20af4d6
Rokas <jasinskas.rokas@gmail.com>
2023-09-13 07:58:16 +0300

    packages update

---
+++ tc/JavaScript_playground/InstallandUsePackage/index.js
@@ -0,0 +1,14 @@
+import { format } from "date-fns";
+import { intersection } from "lodash-es";
+
+const date = new Date();
+const dateFormatted = format(date, "yyyy.MM.dd");
+
+console.log("dateFormatted", dateFormatted);
+
+const words = ["hello", "earth", "hello", "mars"];
+const planets = ["venus", "earth", "mars"];
+
+const planetsMentioned = intersection(words, planets);
+
+console.log("planetsMentioned", planetsMentioned);
