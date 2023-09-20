744dba88ef924b288b95b153a8aa8b8085c0f865
Rokas <jasinskas.rokas@gmail.com>
2023-09-20 21:29:55 +0300

    second vite excercise

--- tc/JavaScript_playground/vite_learning/excersice-vite-2/src/App.vue
+++ tc/JavaScript_playground/vite_learning/excersice-vite-2/src/App.vue
@@ -1,15 +1,48 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Placeholder for paragraph');
+const hover = ref('Message after hover');
+const redtext = ref(true);
+const textcolor = ref('green');
+
+function togglered() {
+  redtext.value = !redtext.value;
+}
+
+function alternate() {
+  textcolor.value = textcolor.value === 'green' ? 'blue' : 'green';
+}
+</script>
 
 <template>
   <header>
     <h1>Hello World!</h1>
   </header>
-
-  <main></main>
+  <main>
+    <div>
+      <span :title="hover">{{ message }}</span>
+    </div>
+    <div>
+      <span :class="{ red: redtext }" @click="togglered" @keyup.enter="togglered">{{
+        message
+      }}</span>
+    </div>
+    <div>
+      <span @click="alternate" :style="{ color: textcolor }" @keyup.enter="alternate">{{
+        message
+      }}</span>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-header {
+header,
+main {
   text-align: center;
+}
+
+.red {
+  color: red;
 }
 </style>
