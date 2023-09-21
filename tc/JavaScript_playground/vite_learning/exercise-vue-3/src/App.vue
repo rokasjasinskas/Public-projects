5cb00d7cdbfe53167730b9212784dc3242bd095f
Rokas <jasinskas.rokas@gmail.com>
2023-09-21 15:54:51 +0300

    edited names

---
+++ tc/JavaScript_playground/vite_learning/exercise-vue-3/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Hello World!</h1>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+</style>
