7af92ab0a01ee512ab50765568124117054aee8e
Rokas <jasinskas.rokas@gmail.com>
2023-09-22 11:08:26 +0300

    y

---
+++ tc/JavaScript_playground/vite_learning/exercise-vue-5/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const products = ref([
+  {
+    text: 'apple',
+    quantity: 5,
+    id: '1'
+  },
+  {
+    text: 'banana',
+    quantity: 3,
+    id: '2'
+  },
+  {
+    text: 'carrot',
+    quantity: 7,
+    id: '3'
+  }
+]);
+
+const groceryitem = ref();
+</script>
+
+<template>
+  <header>
+    <h1>Hello World!</h1>
+  </header>
+  <main>
+    <li v-for="product in products" :key="product.id">
+      {{ product.text }}
+    </li>
+  </main>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+</style>
