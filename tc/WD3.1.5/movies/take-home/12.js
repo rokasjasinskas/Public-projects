a07d24148390da09017cd92466a91d60abe9abe8
Rokas <jasinskas.rokas@gmail.com>
2023-10-22 13:14:33 +0300

    sql

--- tc/WD3.1.5/movies/take-home/12.js
+++ tc/WD3.1.5/movies/take-home/12.js
@@ -1,21 +1,80 @@
 const Database = require('better-sqlite3');
 const db = new Database('../movies.db');
 
-const title = 'from command line';
-const year = 'from command line';
+const title = process.argv[2];
+const year = process.argv[3];
 
-const row = db.prepare('SELECT * FROM people LIMIT 10').all();
-console.log(row);
-
-function check_title () {
-
+function check_argument(title, year) {
+  if (title != null && year != null && typeof process.argv[4] === 'undefined') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-if check_title = TRUE { 
-const add_new_rows = db.prepare('
+function check_title() {
+  const results = db
+    .prepare(
+      `SELECT title FROM movies 
+    WHERE title like @movie_title`
+    )
+    .all({ movie_title: title });
 
-')
+  if (results.length === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
-else{
-    console.log('Error. This title already exists')
+
+function add_new_row(title, year) {
+  db.prepare('BEGIN').run();
+
+  try {
+    const insertMovieStmt = db.prepare(
+      `INSERT INTO movies (title, year)
+         SELECT @movietitle, @movieyear
+         WHERE NOT EXISTS (
+             SELECT title FROM movies 
+             WHERE title LIKE @movietitle)`
+    );
+
+    const info = insertMovieStmt.run({
+      movietitle: title,
+      movieyear: year,
+    });
+
+    if (info.changes) {
+      const movieId = info.lastInsertRowid;
+
+      const insertRatingStmt = db.prepare(
+        `INSERT INTO ratings (movie_id, rating, votes)
+           VALUES (?, 0, 0)`
+      );
+
+      insertRatingStmt.run(movieId);
+
+      console.log('New movie ID:', movieId);
+    }
+
+    db.prepare('COMMIT').run();
+  } catch (err) {
+    db.prepare('ROLLBACK').run();
+    console.error('Transaction failed', err);
+  }
+}
+
+// main
+try {
+  if (check_argument(title, year) === true) {
+    if (check_title() === false) {
+      add_new_row(title, year);
+    } else {
+      console.log('Error. This title already exists');
+    }
+  } else {
+    console.log('Error with console arguments');
+  }
+} catch (error) {
+  console.error(error);
 }
---
+++ tc/WD3.1.5/movies/take-home/12.sql
@@ -0,0 +1,5 @@
+
+SELECT * from movies 
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.id = 22802532;
+
